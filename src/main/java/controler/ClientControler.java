156dd7a7bb5ce51b3ef005eca345ec590f9f5346
RobinHellstrom <robin.hellstrom@kiro.one>
2020-01-07 11:39:23 +0100

    Updaterat av klasser

--- src/main/java/controler/ClientControler.java
+++ src/main/java/controler/ClientControler.java
@@ -5,6 +5,10 @@
  */
 package controler;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 /**
  *
  * @author Caroline
@@ -16,6 +20,13 @@ public class ClientControler {
             + "web/schedule1/ri.json?h=t&sid=3&p=20190902.x,20200906.x&objects=119838.28&ox=0&types=0&fe=0";
     private static final String CANVAS_URI = "https://ltu.instructure.com/api/v1/calendar_events.json";
 
-    
-    
+    private void getTimeEditCalendar() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(ClientControler.TIME_EDIT_URI);
+        
+    }
+
+    public void setCanvasCalendar() {
+
+    }
 }
